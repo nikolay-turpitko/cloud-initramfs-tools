c9a730f3e4fe0cd6c27528f4c7ce00c1c4a70130
Juerg Haefliger <juergh@gmail.com>
2013-02-25 11:48:00 +0100

    move dracut module underneath underneat growroot, add setup script for fedora
    bzr-revno: 84.1.3

---
+++ growroot/dracut/modules.d/50growroot/module-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+check() {
+    return 0
+}
+
+depends() {
+    return 0
+}
+
+install() {
+    inst_hook pre-pivot 00 "$moddir/growroot.sh"
+
+    dracut_install awk
+    dracut_install growpart
+    dracut_install sfdisk
+    dracut_install sgdisk
+}
