f8703b7b4dcfe2dcea11c065f5ce5580f6516067
Juerg Haefliger <juergh@gmail.com>
2013-02-25 11:45:09 +0100

    fix indentation, add support for Fedora
    bzr-revno: 84.1.2

--- epel/modules.d/50growroot/growroot.sh
+++ epel/modules.d/50growroot/growroot.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
-. /lib/dracut-lib.sh
-
 # Environment variables that this script relies upon:
 # - NEWROOT
-# - root
-# - fstype
-# - rflags
+
+. /lib/dracut-lib.sh
 
 _info() {
-    echo "growroot: $@"
+    echo "growroot:" "$*"
 }
 
 _warning() {
-    _info "Warning: $@"
+    echo "growroot Warning:" "$*" >&2
 }
 
 # This will drop us into an emergency shell
 _fatal() {
-    _info "Fatal: $@"
+    echo "growroot Fatal:"  "$*" >&2
     exit 1
 }
 
@@ -26,89 +23,91 @@ _fatal() {
 # at NEWROOT
 _growroot()
 {
-	local rootdev rootdisk partnum out
-	local ROOTMNT ROOT FSTYPE RFLAGS
+    local out rootdev rootmnt rootfs opts unused rootdisk partnum
 
-	# Assign environment variables to local variables
-	ROOTMNT=${NEWROOT}
-	ROOT=${root#*:}
-	FSTYPE=${fstype}
-	RFLAGS=${rflags}
+    # If a file indicates we should do nothing, then just return
+    for file in /var/lib/cloud/instance/root-grown /etc/growroot-disabled \
+        /etc/growroot-grown ; do
+        if [ -f "${NEWROOT}${file}" ] ; then
+            _info "${file} exists, nothing to do"
+            return
+        fi
+    done
 
-        # If a file indicates we should do nothing, then just return
-	for file in /var/lib/cloud/instance/root-grown \
-		/etc/growroot-disabled /etc/growroot-grown; do
-		if [ -f "${ROOTMNT}${file}" ] ; then
-			_info "${file} exists, nothing to do"
-			return
-		fi
-	done
+    # Get the root device, root filesystem and mount options
+    if ! out=$(awk '$2 == mt { print }' "mt=${NEWROOT}" < /proc/mounts) ; then
+	_warning "${out}"
+	return
+    fi
 
-        # Figure out what device ROOT is on
-	rootdev=$(readlink -f ${ROOT})
-	if [ ! -e ${rootdev} ] ; then
-		_warning "Failed to get target of link for ${ROOT}"
-		return
-	fi
+    # Need to do it this way, can't use '<<< "${out}"' since RHEL6 doesn't
+    # seem to understand it
+    read rootdev rootmnt rootfs opts unused <<EOF
+${out}
+EOF
+    if [ -z "{rootdev}" -o -z "${rootmnt}" -o -z "${rootfs}" -o \
+        -z "${opts}" ] ; then
+	_warning "${out}"
+	return
+    fi
 
-        # If the basename of the root device (ie 'xvda1', 'sda1', 'vda') exists
-        # in /sys/block/ then it is a block device, not a partition
-	if [ -e "/sys/block/${rootdev##*/}" ] ; then
-		_info "${rootdev} is not a partition"
-		return
-	fi
+    # If the basename of the root device (ie 'xvda1', 'sda1', 'vda') exists
+    # in /sys/block/ then it is a block device, not a partition
+    if [ -e "/sys/block/${rootdev##*/}" ] ; then
+        _info "${rootdev} is not a partition"
+        return
+    fi
 
-	# Check if the root device is a partition (name ends with a digit)
-	if [ "${rootdev%[0-9]}" = "${rootdev}" ] ; then
-		_warning "${rootdev} is not a partition"
-		return
-	fi
+    # Check if the root device is a partition (name ends with a digit)
+    if [ "${rootdev%[0-9]}" = "${rootdev}" ] ; then
+        _warning "${rootdev} is not a partition"
+        return
+    fi
 
-        # Remove all numbers from the end of rootdev to get the rootdisk and
-	# partition number
-	rootdisk=${rootdev}
-	while [ "${rootdisk%[0-9]}" != "${rootdisk}" ] ; do
-		rootdisk=${rootdisk%[0-9]}
-	done
-	partnum=${rootdev#${rootdisk}}
+    # Remove all numbers from the end of rootdev to get the rootdisk and
+    # partition number
+    rootdisk=${rootdev}
+    while [ "${rootdisk%[0-9]}" != "${rootdisk}" ] ; do
+        rootdisk=${rootdisk%[0-9]}
+    done
+    partnum=${rootdev#${rootdisk}}
 
-        # Do a growpart dry run and exit if it fails or doesn't have
-	# anything to do
-	if ! out=$(growpart --dry-run "${rootdisk}" "${partnum}" 2>&1) ; then
-		_info "${out}"
-		return
-	fi
+    # Do a growpart dry run and exit if it fails or doesn't have anything to do
+    if ! out=$(growpart --dry-run "${rootdisk}" "${partnum}") ; then
+        _info "${out}"
+        return
+    fi
 
-        # There is something to do, unmount and repartition
-	if ! umount "${ROOTMNT}" ; then
-		_warning "Failed to umount ${ROOTMNT}"
-		return
-	fi
+    # There's something to do so unmount and re-partition
+    if ! umount "${NEWROOT}" ; then
+        _warning "Failed to umount ${NEWROOT}"
+        return
+    fi
 
-        # Wait for any of the initial udev events to finish otherwise growpart
-	# might fail
-	udevsettle
+    # Wait for any of the initial udev events to finish otherwise growpart
+    # might fail
+    udevsettle
 
-        # Resize the root partition
-	if out=$(growpart "${rootdisk}" "${partnum}" 2>&1) ; then
-		_info "${out}"
-	else
-		_warning "${out}"
-		_warning "growpart failed"
-	fi
+    # Resize the root partition
+    if out=$(growpart "${rootdisk}" "${partnum}" 2>&1) ; then
+        _info "${out}"
+    else
+        _warning "${out}"
+        _warning "growpart failed"
+    fi
 
-        # Wait for the partition re-read events to complete so that the root
-	# partition is available for remounting
-	udevsettle
+    # Wait for the partition re-read events to complete so that the root
+    # partition is available for remounting
+    udevsettle
 
-        # Remount the root filesystem (from 95rootfs-block/mount-root.sh)
-	mount -t ${FSTYPE:-auto} -o "${RFLAGS}" ${ROOT} ${ROOTMNT} || \
-		_fatal "Failed to re-mount ${ROOT}, this is bad"
+    # Remount the root filesystem
+    mount -t "${rootfs}" -o "${opts}" "${rootdev}" "${NEWROOT}" || \
+        _fatal "Failed to re-mount ${rootdev}, this is bad"
 
-        # Write to /etc/growroot-grown, most likely this wont work (read-only)
-	{
-		date --utc > "${ROOTMNT}/etc/growroot-grown"
-	} >/dev/null 2>&1
+    # Write to /etc/growroot-grown, most likely this wont work (read-only)
+    {
+        date --utc > "${NEWROOT}/etc/growroot-grown"
+    } >/dev/null 2>&1
 }
 
 _growroot
